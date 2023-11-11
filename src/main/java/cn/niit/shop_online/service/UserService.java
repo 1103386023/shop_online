f51d0ab1ffda88c68fc0c90887f1e37b5d954576
1103386023 <1103386023@qq.com>
2023-11-11 12:24:50 +0800

    feat:用户微信登录、用户详情、修改用户信息

--- src/main/java/cn/niit/shop_online/service/UserService.java
+++ src/main/java/cn/niit/shop_online/service/UserService.java
@@ -1,6 +1,9 @@
 package cn.niit.shop_online.service;
 
 import cn.niit.shop_online.entity.User;
+import cn.niit.shop_online.query.LoginResultVO;
+import cn.niit.shop_online.query.UserLoginQuery;
+import cn.niit.shop_online.query.UserVO;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -12,5 +15,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-07
  */
 public interface UserService extends IService<User> {
-
+    LoginResultVO login(UserLoginQuery query);
+    User getUserInfo(Integer userId);
+    UserVO editUserInfo(UserVO userVO);
 }
