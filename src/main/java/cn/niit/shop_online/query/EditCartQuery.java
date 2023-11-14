d8b94ee88745f0a59a23a387131ba3aa832cac65
1103386023 <1103386023@qq.com>
2023-11-14 09:16:32 +0800

    feat:修改购物车单品

---
+++ src/main/java/cn/niit/shop_online/query/EditCartQuery.java
@@ -0,0 +1,18 @@
+package cn.niit.shop_online.query;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class EditCartQuery {
+    @Schema(description = "主键id")
+    @NotNull(message = "主键id不能为空")
+    private String id;
+    @Schema(description = "是否选中")
+    private Boolean selected;
+    @Schema(description = "选购商品数量")
+    private Integer count;
+}
+
